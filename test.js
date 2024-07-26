d46c18d3ada4255a44ecaf8f909e08fa6632b41b
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-26 16:06:29 +0600

    basic class

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+
+class Car {
+    constructor(name, model) {
+        this.name = name;
+        this.brand = model;
+    }
+}
+
+
+
+const car1 = new Car('tesla', 'model3')
+
+console.log(car1.brand);
